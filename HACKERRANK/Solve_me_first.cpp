fdd167f96867f21ec6b49da770c14398126236ae
shivsharcode <138241807+shivsharcode@users.noreply.github.com>
2024-04-22 17:33:08 +0000

    Day-22

---
+++ HACKERRANK/Solve_me_first.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solveMeFirst(int a, int b) {
+    
+    return a + b ;
+  
+}
+
+int main() {
+    int a , b ;
+    cin>>a>>b ;
+    int sum = solveMeFirst(a, b) ;
+    cout<<"Sum = "<< sum <<endl ;
+
+    return 0 ;
+
+}
