777ed02ca2b1ef89a878e6b904b4d5dd1f62656d
shivsharcode <138241807+shivsharcode@users.noreply.github.com>
2024-04-26 20:06:33 +0530

    Commit-1

---
+++ HACKERRANK/Between Two Sets new.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void input(vector<int> arr, int m) ;
+
+int main(){
+    
+    int n, m ;
+    cin>>n>>m ;
+    
+    vector<int> a ;
+    vector<int> b ;
+    
+    input(a, n) ;
+    input(b, m) ;
+    
+    return 0 ;
+}
+
+void input(vector<int> arr, int m){
+    
+    for(int i=0; i<m; i++){
+        
+    }
+}
