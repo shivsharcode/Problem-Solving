555d51be377fbdd9d0ac737784b3247c577e8f08
shivsharcode <138241807+shivsharcode@users.noreply.github.com>
2024-05-10 16:50:09 +0530

    C++ Built-in function
    Using CPP reverse function

---
+++ HACKERRANK/Arrays-DS-second.cpp
@@ -0,0 +1,10 @@
+// ARRAYS -DS PROB, HACKERRANK, 
+
+// APPROACH - USING CPP BUILT-IN FUNCTION
+
+vector<int> reverseArray(vector<int> a) {
+    
+    //using reverse function
+    reverse(a.begin(), a.end()) ;
+    return a ;
+}
