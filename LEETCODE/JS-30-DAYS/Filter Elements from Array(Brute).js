70710af0d15a78eac0143e195d8655c7edc6c7c6
Shiv Shar <138241807+shivsharcode@users.noreply.github.com>
2024-07-06 15:04:31 +0530

    Create Filter Elements from Array(Brute).js
    Approach - Brute force

---
+++ LEETCODE/JS-30-DAYS/Filter Elements from Array(Brute).js
@@ -0,0 +1,21 @@
+// APPROACH - BRUTE FORCE
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+
+var filter = function(arr, fn) {
+    let filteredArr = [] ;
+
+    for(let i=0; i<arr.length; i++){
+
+        if( fn( arr[i], i )){
+            filteredArr.push(arr[i]) ;
+        }
+    }
+
+    return filteredArr ;
+
+};
