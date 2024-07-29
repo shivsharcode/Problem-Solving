27359b37536520ddeb8b343f70d003aad4f63636
Shiv Shar <138241807+shivsharcode@users.noreply.github.com>
2024-07-29 15:30:53 +0530

    Update Strong Password.py

--- HACKERRANK/Strong Password.py
+++ HACKERRANK/Strong Password.py
@@ -1,6 +1,5 @@
 def minimumNumber(n, password):
-    # Return the minimum number of characters to make the password strong
-    
+    # Return the minimum number of characters to make the password strong  
     special_characters = "!@#$%^&*()-+"
     
     countNum = 0
@@ -11,25 +10,18 @@ def minimumNumber(n, password):
     # keeping count of each character in the current Passwrod string
     for i in password:
         if i.isdigit():
-            countNum += 1
+            countNum = 1
         elif i.islower():
-            countLower += 1
+            countLower = 1
         elif i.isupper():
-            countUpper += 1
+            countUpper = 1
         elif i in special_characters:
-            countSpecial += 1
+            countSpecial = 1
             
     # calculating the characters Needed for STRONG Password
-    charNeeded = 0
     
-    if countNum == 0:
-        charNeeded += 1
-    if countLower == 0:
-        charNeeded += 1
-    if countUpper == 0:
-        charNeeded += 1
-    if countSpecial == 0:
-        charNeeded += 1
+    charPresent = countNum + countLower + countUpper + countSpecial
+    charNeeded = 4- charPresent
     
     
     # determing the length condition of password
@@ -41,4 +33,5 @@ def minimumNumber(n, password):
     # returning the character needed
     return charNeeded
     
-  
+    
+    
