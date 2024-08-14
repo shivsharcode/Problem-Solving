03159e0bd1a9f52ce6e5d05993723253c3a02245
Shiv Shar <138241807+shivsharcode@users.noreply.github.com>
2024-08-14 23:42:10 +0530

    Create Overspeeding Fine.py

---
+++ CODECHEF/Overspeeding Fine.py
@@ -0,0 +1,14 @@
+# cook your dish here
+t = int(input())
+
+while t :
+    x = int(input())
+    
+    if x <= 70:
+        print("0")
+    elif x > 70 and x <=100 :
+        print("500")
+    else:
+        print("2000")
+    
+    t-=1    
