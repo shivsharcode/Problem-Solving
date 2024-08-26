6cd305472033ec0b50a9bdaf077cf910b76e79e3
Shiv Shar <138241807+shivsharcode@users.noreply.github.com>
2024-08-26 21:43:46 +0530

    Create lunchtime.py

---
+++ CODECHEF/lunchtime.py
@@ -0,0 +1,13 @@
+# cook your dish here
+t = int(input())
+
+while t:
+    x = int(input())
+    
+    if x >= 1 and x <=4 :
+        print("YES")
+    else:
+        print("NO")
+        
+    
+    t-=1 
